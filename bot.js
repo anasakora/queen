1ef8afe74ca2962e29eb178b19cef9197693ad43
anasakora <53491903+anasakora@users.noreply.github.com>
2019-08-03 17:02:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ console.log("For IIahmad111");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("605336697210798081")
+let channel =     client.channels.get("592008740748656653")
 setInterval(function() {
 channel.send(`#avatar <@584411349233172501> **amwaaaaaa7** :heart_eyes: `);
 }, 30)
